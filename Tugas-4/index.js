e92490700b672c16baed9b5753b9e287d39f1ca4
Marsmalow20 <davidrenaldy1@gmail.com>
2020-10-01 12:26:53 +0700

    revisi jawaban soal nomer 5

--- Tugas-4/index.js
+++ Tugas-4/index.js
@@ -39,12 +39,10 @@ for(i=1; i<=7; i++){
  var arr = kalimat.split(" ");
  console.log(arr);
 
-//  soal 5
+//  soal 5 method
 console.log('\nSoal 5');
 var daftarBuah = ['2. Apel', '5. Jeruk', '3. Anggur', '4. Semangka', '1. Mangga'];
 daftarBuah.sort();
-console.log(daftarBuah[0]);
-console.log(daftarBuah[1]);
-console.log(daftarBuah[2]);
-console.log(daftarBuah[3]);
-console.log(daftarBuah[4]);
+for (i=0; i<=4; i++) {
+    console.log(daftarBuah[i]);
+}
