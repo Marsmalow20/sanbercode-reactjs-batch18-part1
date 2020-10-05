c3cea0ee9812a2bab6d730b731b05367617551c7
Marsmalow20 <davidrenaldy1@gmail.com>
2020-10-05 12:35:58 +0700

    menambah folder tugas-6 [ES6]

---
+++ Tugas-6/index.js
@@ -0,0 +1,73 @@
+//Soal 1
+console.log('Soal 1');
+let sisi = 4;
+let alas = 6;
+let tinggi = 5;
+let r = 7;
+let phi = 3.14;
+
+// myFunction();
+
+const luasPersegi = () => {
+    let luasp = sisi*sisi;
+    console.log("Luas Persegi = "+sisi+" * " +sisi+" = "+luasp);
+}
+
+const luasSegitiga = () => {
+    let luass = (alas*tinggi)/2;
+    console.log("Luas Segitiga = "+alas+" * "+tinggi+ " / 2 = "+luass);
+}
+
+const luasLingkaran = () => {
+    var luasl = phi*r*r;
+    console.log("Luas Lingkaran = "+phi+" * "+r+" * "+r+" = "+luasl);
+}
+
+luasPersegi();
+luasSegitiga();
+luasLingkaran();
+
+//Soal 2
+console.log('\nSoal 2');
+
+const first = 'saya'
+const second = 'adalah'
+const third = 'seorang'
+const fourth = 'frontend'
+const fifth = 'developer'
+ 
+const kalimat = `${first} ${second} ${third} ${fourth} ${fifth}`;
+ 
+console.log(kalimat);
+
+// Soal 3
+console.log('\nSoal 3');
+
+const newFunction = (firstName, lastName) => {
+    console.log(firstName + " " + lastName);
+}
+
+newFunction("William", "Shakespeare");
+
+// Soal 4
+console.log('\nSoal 4');
+
+const newObject = {
+    firstName: "Harry",
+    lastName: "Potter Holt",
+    destination: "Hogwarts React Conf",
+    occupation: "Deve-wizard Avocado",
+    spell: "Vimulus Renderus!!!"
+}
+
+const {firstName, lastName, destination, occupation, spell} = newObject;
+console.log(firstName, lastName, destination, occupation, spell);
+
+// Soal 5
+console.log('\nSoal 5');
+
+const west = ["Will", "Chris", "Sam", "Holly"];
+const east = ["Gill", "Brian", "Noel", "Maggie"];
+const combined = [...west, ...east];
+
+console.log(combined);
